61ab0e4fb37017c4ba3991797b08902b0c307d1c
Mariana Valencia <purple.mv@gmail.com>
2019-10-18 16:21:07 -0500

    Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -14,8 +14,7 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const initialState = useInitialState(API);
   return initialState.length === 0 ? <h1>Loading...</h1> : (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
 
       {initialState.mylist.length > 0 && (
@@ -37,9 +36,7 @@ const Home = () => {
           {initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
-      <Footer />
-
-    </div>
+    </>
   );
 };
 
